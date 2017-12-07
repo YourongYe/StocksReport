a4940345f6976d676b66157d8360b623e6b417de
YourongYe <34338026+YourongYe@users.noreply.github.com>
2017-12-07 10:29:26 +0000

    Update runme.sh

--- runme.sh
+++ runme.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-/home/user/mizhang/miniconda3/bin/python py_screenshot_batch.py
+python py_screenshot_batch.py
 
-/home/user/mizhang/miniconda3/bin/python pdf_report_generator.py
+python pdf_report_generator.py
 
 echo "Sending email"
-echo "Tickers report" | mailx -s "Tickers report" -a "./out/report.pdf" mizhang@factset.com
+echo "Tickers report" | mailx -s "Tickers report" -a "./out/report.pdf" yourong.ye@hotmail.com
